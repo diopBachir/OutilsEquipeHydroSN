e3c1e6a7b2cbbacd232eb6c7cdf3291bacacb61d
Serigne Bassirou Diop <09.bachir.diop10@gmail.com>
2023-01-10 20:45:41 +0000

    initial commit 2

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,12 +20,12 @@
 ##
 golem::fill_desc(
   pkg_name = "OutilsEquipeHydroSN", # The Name of the package containing the App
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
-  repo_url = NULL # The URL of the GitHub Repo (optional)
+  pkg_title = "Create A GUI From Scripts Implemented By The Hydro Team (UGB)", # The Title of the package containing the App
+  pkg_description = "A \"declarative\" programming system for creating hydrological graphics and geoprocessing based on the tools developed by the hydrology team of the Leidi() laboratory of the Gaston Berger University of Saint-Louis (Senegal). You provide the data, and he takes care of the details.", # The Description of the package containing the App
+  author_first_name = "Serigne bassirou", # Your First Name
+  author_last_name = "DIOP", # Your Last Name
+  author_email = "09.bachir.diop.10@gmail.com", # Your Email
+  repo_url = "https://github.com/diopBachir/OutilsEquipeHydro.git" # The URL of the GitHub Repo (optional)
 )
 
 ## Set {golem} options ----
@@ -33,7 +33,7 @@ golem::set_golem_options()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license("Golem User") # You can set another license here
+usethis::use_mit_license() # You can set another license here
 usethis::use_readme_rmd(open = FALSE)
 # Note that `contact` is required since usethis version 2.1.5
 # If your {usethis} version is older, you can remove that param
@@ -54,8 +54,8 @@ golem::use_favicon() # path = "path/to/ico". Can be an online file.
 # golem::remove_favicon() # Uncomment to remove the default favicon
 
 ## Add helper functions ----
-golem::use_utils_ui(with_test = TRUE)
-golem::use_utils_server(with_test = TRUE)
+golem::use_utils_ui()
+golem::use_utils_server()
 
 # You're now set! ----
 
